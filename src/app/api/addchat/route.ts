29387e2e4e87d7739ffe3e277ee5f4d0f7aff6e0
asadbahi <possiblepossible51@gmail.com>
2024-09-17 23:39:09 +0500

    I am preety sure now it wll work

--- src/app/api/addchat/route.ts
+++ src/app/api/addchat/route.ts
@@ -56,14 +56,16 @@ try {
 
  
 
-    const pusher = new Pusher({
-        appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID || "",
-        key: process.env.NEXT_PUBLIC_PUSHER_KEY || "",
-        secret: process.env.PUSHER_SECRET || "",  // Only accessible on the server
-        cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER || "",
-  useTLS: true
-});
+    const Pusher = require('pusher');
 
+    const pusher = new Pusher({
+      appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID,
+      key: process.env.NEXT_PUBLIC_PUSHER_KEY,
+      secret: process.env.PUSHER_SECRET, 
+      cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
+      useTLS: true,
+    });
+    
 
 
 
