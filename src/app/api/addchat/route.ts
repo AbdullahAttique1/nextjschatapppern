7bfcfe177bcfdc15ad8c940cc188e5f25ce307e8
asadbahi <possiblepossible51@gmail.com>
2024-09-18 22:35:18 +0500

    remove pusger

--- src/app/api/addchat/route.ts
+++ src/app/api/addchat/route.ts
@@ -4,11 +4,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 
 
-import  Pusher from "pusher"
-
-
-
-
 
 export  async function POST(req: NextRequest) {
 
@@ -56,35 +51,6 @@ try {
 
 
 
-
-
-
-  
-
-
-
-
-    const pusher = new Pusher({
-      appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID || "",
-      key: process.env.NEXT_PUBLIC_PUSHER_KEY || "",
-      secret: process.env.PUSHER_SECRET || "", 
-      cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER || "",
-      useTLS: true,
-    });
-    
-
-
-
-
-pusher.trigger("my-channel", "my-event", {
-  message: mymsg
-});
-
-
-
-
-    // const io = new Server();
-    // io.emit('message1', 'Message added to the database');
 
     console.log('Message created:', mymsg);
 
